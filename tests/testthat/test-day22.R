61590f7815360d72f52b528b43dbfceed76e0578
TJ Mahr <tjmahr@users.noreply.github.com>
2021-12-23 08:13:30 -0600

    thinking about a compact() step on day 22

--- tests/testthat/test-day22.R
+++ tests/testthat/test-day22.R
@@ -11,3 +11,26 @@ test_that("day 22", {
     f22b_flip_cubes_big() |>
     expect_equal(590784)
 })
+
+test_that("day 22 intervals", {
+  a <- c(-20, 10)
+  b <- c(-20, 10)
+  c <- c( 10, 20)
+  d <- c( 21, 30)
+
+  are_identical <- function(a, b) {
+    all(a == b)
+  }
+
+  are_disjoint <- function(a, b) {
+    s <- unique(sort(c(a, b)))
+    length(s) == 4 &&
+      (all(s[1:2] == a) || all(s[1:2] == b))
+  }
+  are_identical(a, b) |>
+    expect_true()
+  are_disjoint(a, b) |>
+    expect_false()
+
+  # c,d disjoint
+})
